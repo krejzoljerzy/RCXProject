578ccbd2560be7fb0ae5bfde80585db39cb17831
krejzoljerzy <bboguszewicz@gmail.com>
2013-03-27 17:25:40 +0100

    * Added comments in java CompileAndLink project
    * Fresh batch file.

--- sw/CompileAndLink/src/com/BGHK/RCX/CompileAndLink/CompileAndLink.java
+++ sw/CompileAndLink/src/com/BGHK/RCX/CompileAndLink/CompileAndLink.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,26 +34,55 @@ public class CompileAndLink {
 
 	public static void listAllFiles() {
 
+		// Obtain and store path to C project.
 		String pathToCProject = findPathToCProject();
+		File binFolder = new File (pathToCProject+"\\bin");
+		File objFolder = new File (pathToCProject+"\\obj");
+		
+		// Create bin and obj directories if they don't exist.
+		if (!binFolder.exists()){
+			System.out.println("Creating \\bin folder");
+			binFolder.mkdir();
+		}
+		if (!objFolder.exists()){
+			System.out.println("Creating \\obj folder");
+			objFolder.mkdir();
+		}
+		
+		// Prepare dynamic tables to store all directories, and files in C project.
 		List<String> allPaths = new ArrayList<>();
 		List<String> crucialPaths = new ArrayList<>();
 		List<String> sourceFiles = new ArrayList<>();	
+		
+		// Helper index used for path checking.
 		int indexToNotCheckedPath = 1;
-		//
+		
+		// Start searching from root directory of C project.
 		File projectPath = new File(pathToCProject);
+		
+		// List all objects in this directory.
 		String[] list = projectPath.list();
+		
+		// Set parent to none.
 		String parent = "";
+		
+		// Add root path to path list.
 		allPaths.add("\\");
+		
+		// Now search for any subfolder in paths stored in allPaths list.
 		for (int k = 0; k < allPaths.size(); k++) {
 			for (int i = 0; i < list.length; i++) {
+				
 				// If listed item is a directory.
 				if (!list[i].contains(".")) {
 					allPaths.add(parent + "\\" + list[i]);
 				}
 			}
+			// Check if we are not going out of the list area.
 			if(indexToNotCheckedPath<allPaths.size()-1){
 				indexToNotCheckedPath++;
 			}
+			// Save next path from list as a parent and list all objects in it.
 			parent = allPaths.get(indexToNotCheckedPath);
 			list=new File(projectPath+"\\"+parent).list();
 		}
@@ -63,45 +91,64 @@ public class CompileAndLink {
 		for (int i=0;i<allPaths.size();i++){
 			File bufferFile = new File(pathToCProject+allPaths.get(i));
 			String[] bufferPaths = bufferFile.list();
+			
+			// Check if any object in this directory has a source, header or thumb list file.
 			for (int k=0; k<bufferPaths.length;k++){
 				if(bufferPaths[k].contains(".s")||bufferPaths[k].contains(".c")||bufferPaths[k].contains(".h")){
+					
+					// if so, add it to crucialPaths list.
 					crucialPaths.add(allPaths.get(i));
+					
+					// We can now stop looking for other files. Stop loop.
 					k=bufferPaths.length;
 				}
 			}
 		}
 		
 		// Now we've got all crucial paths so let's them rewrite to final String[] and find all files to compile.
+		// In the same time look for any source file in crucial directories.
 		for(int i=0; i<crucialPaths.size();i++){
 			
 			
 			File bufferFile= new File(pathToCProject+crucialPaths.get(i));
 			String[] listBuffer = bufferFile.list();
+			
+			// Look for any source file in this path except *.coproj files and store it in the list.
 			for(int k=0;k<listBuffer.length;k++){
 				if((listBuffer[k].contains(".c")||listBuffer[k].contains(".s"))&& !listBuffer[k].contains(".coproj")){
 					sourceFiles.add(crucialPaths.get(i)+"\\"+listBuffer[k]);
 				}
 			}
 		}
+		// Save crucial paths and source files lists in global String arrays.
 		includePaths = crucialPaths.toArray(new String[crucialPaths.size()]);
 		filesToCompile = sourceFiles.toArray(new String[sourceFiles.size()]);
 
 	}
 
 	private static String findPathToCProject() {
+		
+		// Get path to java project.
 		String actualPath = CompileAndLink.class.getClassLoader()
 				.getResource(".").getPath();
+		
+		// Get path to main repo.
 		String stringBufferArray[] = actualPath.split("/sw");
-
+		
+		// Add local path to C project to repo path.
 		String pathToCProject = stringBufferArray[0] + "\\fw\\legoRCX";
+		
+		// Replace all slashes with backslashes.
 		pathToCProject = pathToCProject.replace("/", "\\");
+		
+		// Remove first backslash.
 		pathToCProject = pathToCProject.substring(1);
 		return pathToCProject;
 	}
 
 	public static void createBatFile() throws IOException {
 
-		// Create new bat file.
+		// Create new bat file, remove if it existed before.
 		File f;
 		String wholeIncludes = "";
 		f = new File("compilerCommand.bat");
@@ -110,9 +157,10 @@ public class CompileAndLink {
 		}
 		f.createNewFile();
 
+		// Get path to C project.
 		String pathToCProject = findPathToCProject();
-		System.out.println(pathToCProject);
 
+		// Make all paths global, add include command and store them in one String.
 		for (int i = 0; i < includePaths.length; i++) {
 
 			includePaths[i] = pathToCProject + includePaths[i];
@@ -122,11 +170,14 @@ public class CompileAndLink {
 		}
 
 		try {
-			// Write to file
+			// Open and write to batch file.
 			FileWriter fstream = new FileWriter("compilerCommand.bat");
 			BufferedWriter out = new BufferedWriter(fstream);
+			
+			// Create String array with all files to compile.
 			String[] fileName = new String[filesToCompile.length];
-			// Write compilation commands to batch file.
+			
+			// Write compilation commands to batch file (source and output).
 			for (int i = 0; i < filesToCompile.length; i++) {
 				String buffer = null;
 				buffer = compileCommands;
@@ -149,7 +200,7 @@ public class CompileAndLink {
 				out.write(buffer);
 			}
 
-			// Link objects.
+			// Create linker script with all object files objects.
 			String buffer = "";
 			buffer += linkerCommand;
 			buffer += " -Wl,-Map=" + pathToCProject + "\\obj\\" + mapFile;
@@ -160,12 +211,20 @@ public class CompileAndLink {
 			for (int i = 0; i < fileName.length; i++) {
 				buffer += " " + pathToCProject + "\\obj\\" + fileName[i];
 			}
+			
+			// Write to batch file
 			out.write(buffer);
-
+			
+			// Write closing command to file
 			out.write("\r\nexit");
+			
 			// Close the output stream
 			out.close();
-		} catch (Exception e) {// Catch exception if any
+			
+		}
+		
+		// Catch exception if any
+		catch (Exception e) {
 			System.err.println("Error: " + e.getMessage());
 		}
 
