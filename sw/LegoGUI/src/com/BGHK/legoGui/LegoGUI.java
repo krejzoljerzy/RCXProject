7f87561c795f7085a184cc45727b300ac5c360ab
krejzoljerzy <bboguszewicz@gmail.com>
2013-05-04 21:25:24 +0200

    btm 222 eagle library

---
+++ sw/LegoGUI/src/com/BGHK/legoGui/LegoGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BGHK.legoGui;
+
+/**
+ *
+ * @author johns
+ */
+public class LegoGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Window window = new Window();
+        window.startWindow();
+        // TODO code application logic here
+    }
+}
