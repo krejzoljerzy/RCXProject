49217464ef0c5aa4b505c24c73cd5f7964d87a67
krejzoljerzy <bboguszewicz@gmail.com>
2013-11-30 18:04:37 +0100

    SW:
     * Got the blocks zooming and moving

--- sw/LegoGUI/src/com/BGHK/elements/Root.java
+++ sw/LegoGUI/src/com/BGHK/elements/Root.java
@@ -22,31 +22,37 @@ import javax.swing.JPanel;
  *
  * @author johns
  */
-public class Root extends Block {
+public class Motors extends Block {
 
     JLabel title;
     JLabel power;
     JLabel time;
-    int motorNumber = 1;
-    int powerVal = 100;
-    int timeVal = 2;
-    int paramNum = 3;
-    String titleStr = "";
+    int motorNumber=1;
+    int powerVal=100;
+    int timeVal=2;
+    int paramNum =3;
 
-    public Root(Color clr, String name) {
+    String titleStr = "Silnik " + Integer.toString(motorNumber);
+    String param1Str = Integer.toString(powerVal)+ " %";
+    String param2Str = Integer.toString(timeVal)+ "s";
 
+    public Motors(Color clr, int number) {
         super(clr);
-        sizeX = 100;
-        sizeY = 150;
+        param1Str = Integer.toString (id);
+        param2Str = Integer.toString(parentId);
         addMouseListener(this);
         Window.myPanel1.add(this);
         Window.myPanel1.repaint();
-        titleStr = name;
+        motorNumber=number;
         title = new JLabel(titleStr, JLabel.CENTER);
-        title.setFont(new Font("Tahoma", Font.BOLD, 23));
-        title.setText(titleStr);
+        title.setFont(new Font ("Tahoma", Font.BOLD, 23));
+        power = new JLabel(param1Str, JLabel.CENTER);
+        power.setFont(new Font ("Tahoma", Font.PLAIN, 20));
+        time = new JLabel(param2Str, JLabel.CENTER);
+        time.setFont(new Font ("Tahoma", Font.PLAIN, 20));
+        //motorNumber = new JLabel(Integer.toString(number), JLabel.CENTER);
 
-        this.setLayout(new GridLayout(paramNum, 0));
+        this.setLayout(new GridLayout(paramNum,0));
         this.add(title);
         this.add(power);
         this.add(time);
@@ -55,20 +61,28 @@ public class Root extends Block {
     }
 
     private void resizeText() {
-        title.setFont(new Font("Tahoma", Font.BOLD, (int) (23 * Window.scale)));
+        title.setFont(new Font ("Tahoma", Font.BOLD, (int)(23*Window.scale)));
+        power.setFont(new Font ("Tahoma", Font.PLAIN, (int)(20*Window.scale)));
+        time.setFont(new Font ("Tahoma", Font.PLAIN, (int)(20*Window.scale)));
         this.validate();
     }
-
+    
     @Override
     public void mousePressed(MouseEvent me) {
         super.mousePressed(me);
-
+      
         // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    public void refresh() {
+    
+    
+    public void refresh(){
         resizeText();
+        titleStr = "Silnik " + Integer.toString(motorNumber);
+        param1Str = Integer.toString (id);
+        param2Str = Integer.toString(parentId);
         title.setText(titleStr);
+        power.setText(param1Str);
+        time.setText(param2Str);
         this.validate();
     }
 }
